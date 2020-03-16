91cd97b383b9c0e77ba940203e2f647b8e5ef16c
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2020-03-16 15:35:21 -0300

    added graphs to styled dashboard page

--- src/containers/AnalyticsPage/index.js
+++ src/containers/AnalyticsPage/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { GoogleProvider, GoogleDataChart } from 'react-analytics-widget'
+import ResponsiveDrawer from '../MenuDrawer';
 
 
 // graph 1 config
@@ -7,7 +8,7 @@ const last30days = {
     reportType: "ga",
     query: {
         dimensions: "ga:date",
-        metrics: "ga:pageviews",
+        metrics: "ga:pageviews,ga:sessions",
         "start-date": "30daysAgo",
         "end-date": "yesterday"
     },
@@ -26,7 +27,7 @@ const last7days = {
     reportType: "ga",
     query: {
         dimensions: "ga:date",
-        metrics: "ga:pageviews",
+        metrics: "ga:pageviews, ga:sessions",
         "start-date": "7daysAgo",
         "end-date": "yesterday"
     },
@@ -35,6 +36,55 @@ const last7days = {
     }
 }
 
+const trafficByPlatformLast30Days =  {
+  query: {
+      'start-date': "30daysAgo",
+      'end-date': 'yesterday',
+      'metrics': 'ga:pageviews',
+      'dimensions': 'ga:pagePathLevel1',
+      'sort': '-ga:pageviews',
+      'filters': 'ga:pagePathLevel1!=/',
+      'max-results': 4 //number of platforms
+  },
+  chart: {
+      'type': 'PIE',
+      'options': {
+          'width': '100%',
+          'pieHole': 4 / 9,
+      }
+  }
+}
+
+const trafficByPlatformLast7Days =  {
+  query: {
+      'start-date': '7daysAgo',
+      'end-date': 'yesterday',
+      'metrics': 'ga:pageviews, ga:pagePathLevel1',
+      'dimensions': 'ga:date',
+      'sort': '-ga:pageviews',
+      'filters': 'ga:pagePathLevel1!=/',
+      'max-results': 4 //number of platforms
+  },
+  chart: {
+      'type': 'LINE',
+      // 'options': {
+      //     'width': '100%',
+      //     'pieHole': 4 / 9,
+      // }
+  }
+}
+
+const trafficByPlatformOverYears =  {
+  query: {
+    'start-date' : '2016-01-01', 
+    'end-date': 'yesterday',
+    'metrics': 'ga:sessions', 
+    'dimensions': 'ga:year',
+  }, 
+  chart: {
+    'type': 'LINE',
+  }
+}
 // analytics views ID
 const views = {
     query: {
@@ -43,24 +93,26 @@ const views = {
 }
 
 export default class AnalyticsPage extends Component {
-    // componentDidMount = () => {
-    //     const request = new Request('https://yourserver.example/auth/ganalytics/getToken', {
-    //         method: 'GET'
-    //     });
-    //     fetch(request)
-    //         .then(response => response.json())
-    //         .then(({ token }) => {
-    //             this.setState({ token }); // TODO: handle errors
-    //         });
-    // }
     constructor(props) {
         super(props)
-        this.state = { token: "token" }
+        this.state = { token: "ya29.c.Ko4BwgdDQ9YdZjpnOsXbwPyoinoCs98Zi5wGek40kUJqW7VDt0gPODh97Z_IZreP8NI8CPl8AFL5og6NDxl8x9mwOItvxoFjohQ_Qybv_pZ8M8vEOrLKyCEhLcYAG6jJPKievVhFoAP_Tuyusv6h_hT0POg5eC-psEhUjO1w_-AsvGuQscnRxvBK2wx93FKi0w" }
     }
     render = () => (
-        <GoogleProvider accessToken={this.state.token}>
-            <GoogleDataChart views={views} config={last30days} />
-            <GoogleDataChart views={views} config={last7days} />
-        </GoogleProvider>
+        <ResponsiveDrawer title = 'Dashboard'>
+            <center>
+                <GoogleProvider accessToken={this.state.token}>
+                    <h2> Pageviews E-democracia - últimos 30 dias </h2>
+                    <GoogleDataChart views={views} config={last30days} />
+                    <h2> Tráfego por plataforma nos últimos 30 dias</h2>
+                    <GoogleDataChart views={views} config={trafficByPlatformLast30Days} />
+                    <h2> Pageviews E-democracia </h2>
+                    <GoogleDataChart views={views} config={last7days} />
+                    <h2> Tráfego por plataforma nos últimos 7 dias</h2>
+                    <GoogleDataChart views={views} config={trafficByPlatformLast7Days} />
+                    <h2> Tráfego por plataforma nos últimos anos</h2>
+                    <GoogleDataChart views={views} config={trafficByPlatformOverYears} />
+                </GoogleProvider>
+            </center>
+        </ResponsiveDrawer>
     )
 }
