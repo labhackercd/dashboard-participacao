b78bdd9a63f84726586197debd912d039dbc47a5
erivanio <erivanio.vanin@gmail.com>
2020-03-19 15:52:27 -0300

    create form to filter range date in ga data

--- src/containers/AnalyticsPage/index.js
+++ src/containers/AnalyticsPage/index.js
@@ -82,41 +82,6 @@ const trafficByPlatformLast7Days = {
     }
 }
 
-const lastYears = {
-    query: {
-        'start-date': '2016-01-01',
-        'end-date': 'yesterday',
-        'metrics': 'ga:pageviews,ga:sessions',
-        'dimensions': 'ga:year',
-    },
-    chart: {
-        'type': 'LINE',
-        'options': {
-            'title': 'E-democracia - últimos anos'
-        }
-    }
-}
-
-const trafficByPlatformOverYears = {
-    query: {
-        'start-date': '2016-01-01',
-        'end-date': 'yesterday',
-        'metrics': 'ga:pageviews',
-        'dimensions': 'ga:pagePathLevel1',
-        'sort': '-ga:pageviews',
-        'filters': 'ga:pagePathLevel1!=/',
-        'max-results': 4 //number of platforms
-    },
-    chart: {
-        'type': 'PIE',
-        'options': {
-            'width': '100%',
-            'pieHole': 4 / 9,
-            'title': 'Tráfego por plataforma  nos últimos anos'
-        }
-    }
-}
-
 // analytics views ID
 const views = {
     query: {
@@ -124,100 +89,141 @@ const views = {
     }
 }
 
+function getCurrentDate() {
+    let newDate = new Date()
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+
+    return `${year}-${month < 10 ? `0${month}` : `${month}`}-${date}`
+}
+
 export default class AnalyticsPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            token: "ya29.c.Ko4Bwwe3B9xDW2o7QAMnJF4bf89GEgGVLDEMG0jgmQoopa0_jgajs9IwAa676TR1S9xgg0DKb6oxjk7yDRqIrPMQHMJ08tPcQZ90kgU34gdW2CG5rGehYaH60LSWBGqlTnBHFcBWKyHcAFrz6gcDICWaOgyydnNSIfmdJ-LG89Okpq0oBmmKshu7xMb0roMDyw",
-            startDate: '2019-01-01',
-            endDate: '2020-01-01',
-            trafficData: ''
+            token: "ya29.c.Ko4BwwfD-I4QxDqygwEsvGjnOBZmLbl8XT5LiGvaVQW_MIl-tAtCkq2Gy7lIS5tP4aOAKpIDXFt_lPcwH9Ou_nSYunqRhBiYCa3GoqPwUoODS1yWv6b8rIVRYqbP8od3h6MXoDKiW_8P-bVu1n1osSUh40ASvLRM1m37FcfseJw19_CnGZZ2305A1oWDnkldrw",
+            startDate: '2016-01-01',
+            endDate: getCurrentDate(),
+            lineChartData: {
+                query: {
+                    'start-date': '2016-01-01',
+                    'end-date': 'yesterday',
+                    'metrics': 'ga:pageviews,ga:sessions',
+                    'dimensions': 'ga:yearMonth',
+                },
+                chart: {
+                    'type': 'LINE',
+                    'options': {
+                        'title': 'E-democracia - últimos anos'
+                    }
+                }
+            },
+            pieChartData: {
+                query: {
+                    'start-date': '2016-01-01',
+                    'end-date': 'yesterday',
+                    'metrics': 'ga:pageviews',
+                    'dimensions': 'ga:pagePathLevel1',
+                    'sort': '-ga:pageviews',
+                    'filters': 'ga:pagePathLevel1!=/',
+                    'max-results': 4 //number of platforms
+                },
+                chart: {
+                    'type': 'PIE',
+                    'options': {
+                        'width': '100%',
+                        'pieHole': 4 / 9,
+                        'title': 'Tráfego por plataforma  nos últimos anos'
+                    }
+                }
+            },
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleStartDateChange = this.handleStartDateChange.bind(this);
         this.handleEndDateChange = this.handleEndDateChange.bind(this);
-        this.setTrafficByPlatformRangeDate = this.setTrafficByPlatformRangeDate.bind(this);
-    }
-
-    setTrafficByPlatformRangeDate() {
-        var trafficByPlatform = {
-            query: {
-                'start-date': this.state.startDate,
-                'end-date': this.state.endDate,
-                'metrics': 'ga:pageviews',
-                'dimensions': 'ga:pagePathLevel1',
-                'sort': '-ga:pageviews',
-                'filters': 'ga:pagePathLevel1!=/',
-                'max-results': 4 //number of platforms
-            },
-            chart: {
-                'type': 'PIE',
-                'options': {
-                    'width': '100%',
-                    'pieHole': 4 / 9,
-                    'title': 'Tráfego por plataforma  nos últimos anos'
-                }
-            }
-        }
-        this.setState({ trafficData: trafficByPlatform });
     }
 
     handleSubmit(event) {
-        this.setTrafficByPlatformRangeDate(event.target.startDate, event.target.endDate)
+        this.setState(prevState => ({
+            lineChartData: {
+                ...prevState.lineChartData,
+                query: {
+                    ...prevState.query,
+                    'start-date': this.state.startDate,
+                    'end-date': this.state.endDate,
+                    'metrics': 'ga:pageviews,ga:sessions',
+                    'dimensions': 'ga:yearMonth',
+                }
+            },
+            pieChartData: {
+                ...prevState.pieChartData,
+                query: {
+                    ...prevState.query,
+                    'start-date': this.state.startDate,
+                    'end-date': this.state.endDate,
+                    'metrics': 'ga:pageviews',
+                    'dimensions': 'ga:pagePathLevel1',
+                    'sort': '-ga:pageviews',
+                    'filters': 'ga:pagePathLevel1!=/',
+                    'max-results': 4
+                }
+            }
+        }))
         event.preventDefault();
     }
 
-    handleStartDateChange(event) {
-        this.setState({ startDate: event.toISOString().split('T')[0] })
+    handleStartDateChange(date) {
+        this.setState({ startDate: date.toISOString().split('T')[0] })
     }
 
-    handleEndDateChange(event) {
-        this.setState({ endDate: event.toISOString().split('T')[0] })
+    handleEndDateChange(date) {
+        this.setState({ endDate: date.toISOString().split('T')[0] })
     }
 
-    render = () => (
-        <ResponsiveDrawer title='Google Analytics'>
-            <center>
-                <GoogleProvider accessToken={this.state.token}>
-                    <GoogleDataChart views={views} config={last30days} />
-                    <GoogleDataChart views={views} config={trafficByPlatformLast30Days} />
-                    <GoogleDataChart views={views} config={last7days} />
-                    <GoogleDataChart views={views} config={trafficByPlatformLast7Days} />
-                    <GoogleDataChart views={views} config={lastYears} />
-                    <GoogleDataChart views={views} config={trafficByPlatformOverYears} />
-                    <form onSubmit={this.handleSubmit}>
-                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <DatePicker
-                                id="start-date"
-                                label="Start Date"
-                                type="date"
-                                format="yyyy-MM-dd"
-                                value={this.state.startDate}
-                                onChange={this.handleStartDateChange}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                            />
-                            <DatePicker
-                                id="end-date"
-                                label="End Date"
-                                type="date"
-                                format="yyyy-MM-dd"
-                                value={this.state.endDate}
-                                onChange={this.handleEndDateChange}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                            />
-                        </MuiPickersUtilsProvider>
-                        <input type="submit" value="Enviar" />
-                    </form>
-                    <GoogleDataChart views={views} config={this.state.trafficData} />
-                </GoogleProvider>
-
-
-
-            </center>
-        </ResponsiveDrawer>
-    )
+    render() {
+        return (
+            <ResponsiveDrawer title='Google Analytics'>
+                <center>
+                    <GoogleProvider accessToken={this.state.token}>
+                        <GoogleDataChart views={views} config={last30days} />
+                        <GoogleDataChart views={views} config={trafficByPlatformLast30Days} />
+                        <GoogleDataChart views={views} config={last7days} />
+                        <GoogleDataChart views={views} config={trafficByPlatformLast7Days} />
+                        <form onSubmit={this.handleSubmit}>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <DatePicker
+                                    id="start-date"
+                                    name="startDate"
+                                    label="Start Date"
+                                    type="date"
+                                    format="yyyy-MM-dd"
+                                    value={this.state.startDate}
+                                    onChange={this.handleStartDateChange}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <DatePicker
+                                    id="end-date"
+                                    name="endDate"
+                                    label="End Date"
+                                    type="date"
+                                    format="yyyy-MM-dd"
+                                    value={this.state.endDate}
+                                    onChange={this.handleEndDateChange}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                            </MuiPickersUtilsProvider>
+                            <input type="submit" value="Enviar" />
+                        </form>
+                        <GoogleDataChart views={views} config={this.state.lineChartData} />
+                        <GoogleDataChart views={views} config={this.state.pieChartData} />
+                    </GoogleProvider>
+                </center>
+            </ResponsiveDrawer>
+        )
+    }
 }
