b22c032d263896ef6fd1b45f9c614d763e3e8fd4
erivanio <erivanio.vanin@gmail.com>
2020-03-20 15:42:07 -0300

    request ga access_token from another server

--- src/containers/AnalyticsPage/index.js
+++ src/containers/AnalyticsPage/index.js
@@ -103,11 +103,10 @@ function getCurrentDate() {
 }
 
 export default class AnalyticsPage extends Component {
-  
     constructor(props) {
         super(props)
         this.state = {
-            token: "ya29.c.Ko4BwwdTiyaGqpzPa6eM5BYpygrtmSsu9agw1ftpvn-QgzE0DXZBsvHxX47lHXc2PqKVg_rLTqJjgLD58IoIHKQINotlPgMken38DyQLnTy5CUpl3qr8KCSvo8SA-X3lESf5HFKnWZ-FrCR5Llf88SFYx3YIfJPXEQ7OpS0-_c7S0jvml6m_Xy5V40Q5vdghkw",
+            token: "",
             startDate: '2016-01-01',
             endDate: getCurrentDate(),
             lineChartData: {
@@ -115,7 +114,7 @@ export default class AnalyticsPage extends Component {
                     'start-date': '2016-01-01',
                     'end-date': 'yesterday',
                     'metrics': 'ga:pageviews,ga:sessions',
-                    'dimensions': 'ga:yearMonth',
+                    'dimensions': 'ga:year',
                 },
                 chart: {
                     'type': 'LINE',
@@ -146,7 +145,7 @@ export default class AnalyticsPage extends Component {
         };
         this.chartIDs = ['edemocracia-30days', 'edemocracia-7days', 'trafficByPlatformLast7Days']
         this.singleImagePromiseById = this.singleImagePromiseById.bind(this)
-        this.mountsAllPromisesFromSingleImages = this.mountsAllPromisesFromSingleImages .bind(this)
+        this.mountsAllPromisesFromSingleImages = this.mountsAllPromisesFromSingleImages.bind(this)
         this.appendsImagesToPDFFile = this.appendsImagesToPDFFile.bind(this)
         this.generatesAllChartsPDF = this.generatesAllChartsPDF.bind(this)
         this.exportFinalFile = this.exportFinalFile.bind(this)
@@ -155,57 +154,68 @@ export default class AnalyticsPage extends Component {
         this.handleEndDateChange = this.handleEndDateChange.bind(this);
     }
 
+    componentDidMount = () => {
+        const url = new URL('http://localhost:5000')
+        fetch(url, {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then((fetchToken) => {
+                this.setState({ token: fetchToken.token }); // TODO: handle errors
+            });
+    }
+
     singleImagePromiseById(id) {
-      const input = document.getElementById(id)
-      return html2canvas(input).then((canvas) => {
-        return canvas.toDataURL('image.png')
-      })
-    } 
+        const input = document.getElementById(id)
+        return html2canvas(input).then((canvas) => {
+            return canvas.toDataURL('image.png')
+        })
+    }
 
     mountsAllPromisesFromSingleImages() {
-      var promises = []
-      for (var i=0; i < this.chartIDs.length; i++) {
-        var p = this.singleImagePromiseById(this.chartIDs[i])
-        promises.push(p)
-      }
-      return promises
+        var promises = []
+        for (var i = 0; i < this.chartIDs.length; i++) {
+            var p = this.singleImagePromiseById(this.chartIDs[i])
+            promises.push(p)
+        }
+        return promises
     }
 
     generatesAllChartsPDF() {
-      var promises = this.mountsAllPromisesFromSingleImages()
-      Promise.all(promises).then((images) => {
-        this.appendsImagesToPDFFile(images)
-      })
+        var promises = this.mountsAllPromisesFromSingleImages()
+        Promise.all(promises).then((images) => {
+            this.appendsImagesToPDFFile(images)
+        })
     }
 
     appendsImagesToPDFFile(images, single_image_id) {
-      const pdf = new jsPDF('p', 'px', 'a4')
-      var width = pdf.internal.pageSize.getWidth()
-      console.log("images length: " + images.length)
-      images.forEach((image, index) => {
-          var imgProps = pdf.getImageProperties(image)
-          var height = (imgProps.height * width) / imgProps.width
-          pdf.addImage(image, 'JPEG', 0, 0, width, height)
-          if (index < images.length - 1) pdf.addPage()
-      })
-      single_image_id === null ? this.exportFinalFile({
-        'name' : 'all_charts.pdf',
-        'pdf': pdf
-      }) : this.exportFinalFile({
-        'name' : single_image_id,
-        'pdf': pdf
-      })
+        const pdf = new jsPDF('p', 'px', 'a4')
+        var width = pdf.internal.pageSize.getWidth()
+        console.log("images length: " + images.length)
+        images.forEach((image, index) => {
+            var imgProps = pdf.getImageProperties(image)
+            var height = (imgProps.height * width) / imgProps.width
+            pdf.addImage(image, 'JPEG', 0, 0, width, height)
+            if (index < images.length - 1) pdf.addPage()
+        })
+        single_image_id === null ? this.exportFinalFile({
+            'name': 'all_charts.pdf',
+            'pdf': pdf
+        }) : this.exportFinalFile({
+            'name': single_image_id,
+            'pdf': pdf
+        })
     }
 
     exportFinalFile(obj) {
-      obj.pdf.save(obj.name)
+        obj.pdf.save(obj.name)
     }
 
     printSingleChart(id) {
-      this.singleImagePromiseById(id)
-          .then((image) => {
-            this.appendsImagesToPDFFile([image], id)
-        })
+        this.singleImagePromiseById(id)
+            .then((image) => {
+                this.appendsImagesToPDFFile([image], id)
+            })
     }
 
     handleSubmit(event) {
@@ -247,39 +257,31 @@ export default class AnalyticsPage extends Component {
 
     render() {
         return (
-        <ResponsiveDrawer title = 'Dashboard'>
-            <center>
-                <GoogleProvider accessToken={this.state.token}>
-                    <h2> Pageviews E-democracia - últimos 30 dias </h2>
-                    <div className="chart" id='edemocracia-30days'>
-                      <GoogleDataChart views={views} config={last30days} />
-                    </div>
-                </GoogleProvider>
-              <button onClick={() => this.printSingleChart('edemocracia-30days')}> Download </button>
-            </center>
-            <br />
-            <center>
-                <GoogleProvider accessToken={this.state.token}>
-                    <h2> Pageviews E-democracia - últimos 7 dias </h2>
-                    <div className="chart" id='edemocracia-7days'>
-                      <GoogleDataChart views={views} config={last7days} />
-                    </div>
-                </GoogleProvider>
-              <button onClick={() => this.printSingleChart('edemocracia-7days')}> Download </button>
-            </center>
-            <br />
-            <center>
-                <GoogleProvider accessToken={this.state.token}>
-                    <h2> Pageviews E-democracia - últimos 30 dias </h2>
-                    <div className="chart" id='trafficByPlatformLast7Days'>
-                      <GoogleDataChart views={views} config={trafficByPlatformLast30Days} />
-                    </div>
-                </GoogleProvider>
-              <button onClick={() => this.printSingleChart('trafficByPlatformLast7Days')}> Download </button>
-            </center>
-            <br />
-            <form onSubmit={this.handleSubmit}>
-                  <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <ResponsiveDrawer title='Dashboard'>
+                <center>
+                    <GoogleProvider accessToken={this.state.token}>
+                        <h2> E-democracia - últimos 30 dias </h2>
+                        <div className="chart" id='edemocracia-30days'>
+                            <GoogleDataChart views={views} config={last30days} />
+                        </div>
+                        <button onClick={() => this.printSingleChart('edemocracia-30days')}> Download </button>
+                        <div className="chart" id='trafficByPlatformLast30Days'>
+                            <GoogleDataChart views={views} config={trafficByPlatformLast30Days} />
+                        </div>
+                        <br />
+                        <h2> E-democracia - últimos 7 dias </h2>
+                        <div className="chart" id='edemocracia-7days'>
+                            <GoogleDataChart views={views} config={last7days} />
+                        </div>
+                        <button onClick={() => this.printSingleChart('edemocracia-7days')}> Download </button>
+                        <div className="chart" id='trafficByPlatformLast7Days'>
+                            <GoogleDataChart views={views} config={trafficByPlatformLast7Days} />
+                        </div>
+                        <button onClick={() => this.printSingleChart('trafficByPlatformLast7Days')}> Download </button>
+                        <br />
+                        <h2> Selecione o período para consulta </h2>
+                        <form onSubmit={this.handleSubmit}>
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
                                 <DatePicker
                                     id="start-date"
                                     name="startDate"
@@ -308,9 +310,11 @@ export default class AnalyticsPage extends Component {
                             <input type="submit" value="Enviar" />
                         </form>
                         <GoogleDataChart views={views} config={this.state.lineChartData} />
-                        <GoogleDataChart views={views} config={this.state.pieChartData} />            
-            <button onClick={this.generatesAllChartsPDF}> Download All </button>
-        </ResponsiveDrawer>
-      )
+                        <GoogleDataChart views={views} config={this.state.pieChartData} />
+                        <button onClick={this.generatesAllChartsPDF}> Download All </button>
+                    </GoogleProvider>
+                </center>
+            </ResponsiveDrawer >
+        )
     }
-  }
+}
