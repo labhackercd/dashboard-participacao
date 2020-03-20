a2390adfa99bb411c532bf2946d8a37b3724a0e0
erivanio <erivanio.vanin@gmail.com>
2020-03-20 16:47:19 -0300

    remove buttons do export to pdf and improve styles

--- src/containers/AnalyticsPage/index.js
+++ src/containers/AnalyticsPage/index.js
@@ -1,320 +1,276 @@
 import React, { Component } from 'react';
 import { GoogleProvider, GoogleDataChart } from 'react-analytics-widget'
 import ResponsiveDrawer from '../MenuDrawer';
-import * as jsPDF from 'jspdf';
-import * as html2canvas from 'html2canvas';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 
 // graph 1 config q   
 import DateFnsUtils from '@date-io/date-fns';
 import {
-    MuiPickersUtilsProvider,
-    DatePicker,
+  MuiPickersUtilsProvider,
+  DatePicker,
 } from '@material-ui/pickers';
 
 // graph 1 config
 const last30days = {
-    reportType: 'ga',
-    query: {
-        'dimensions': 'ga:date',
-        'metrics': 'ga:pageviews,ga:sessions',
-        'start-date': '30daysAgo',
-        'end-date': 'yesterday'
-    },
-    chart: {
-        type: 'LINE',
-        options: {
-            'title': 'E-democracia - últimos 30 dias'
-        }
+  reportType: 'ga',
+  query: {
+    'dimensions': 'ga:date',
+    'metrics': 'ga:pageviews,ga:sessions',
+    'start-date': '30daysAgo',
+    'end-date': 'yesterday'
+  },
+  chart: {
+    type: 'LINE',
+    options: {
+      'title': 'Acessos nos últimos 30 dias'
     }
+  }
 }
 
 const trafficByPlatformLast30Days = {
-    query: {
-        'start-date': '30daysAgo',
-        'end-date': 'yesterday',
-        'metrics': 'ga:pageviews',
-        'dimensions': 'ga:pagePathLevel1',
-        'sort': '-ga:pageviews',
-        'filters': 'ga:pagePathLevel1!=/',
-        'max-results': 4 //number of platforms
-    },
-    chart: {
-        'type': 'PIE',
-        'options': {
-            'width': '100%',
-            'pieHole': 4 / 9,
-            'title': 'Tráfego por plataforma nos últimos 30 dias'
-        }
+  query: {
+    'start-date': '30daysAgo',
+    'end-date': 'yesterday',
+    'metrics': 'ga:pageviews',
+    'dimensions': 'ga:pagePathLevel1',
+    'sort': '-ga:pageviews',
+    'filters': 'ga:pagePathLevel1!=/',
+    'max-results': 4 //number of platforms
+  },
+  chart: {
+    'type': 'PIE',
+    'options': {
+      'width': '100%',
+      'pieHole': 4 / 9,
+      'title': 'Tráfego por plataforma nos últimos 30 dias'
     }
+  }
 }
 
 // graph 2 config
 const last7days = {
-    reportType: 'ga',
-    query: {
-        'dimensions': 'ga:date',
-        'metrics': 'ga:pageviews,ga:sessions',
-        'start-date': '7daysAgo',
-        'end-date': 'yesterday'
-    },
-    chart: {
-        type: 'LINE',
-        options: {
-            'title': 'E-democracia - últimos 7 dias'
-        }
+  reportType: 'ga',
+  query: {
+    'dimensions': 'ga:date',
+    'metrics': 'ga:pageviews,ga:sessions',
+    'start-date': '7daysAgo',
+    'end-date': 'yesterday'
+  },
+  chart: {
+    type: 'LINE',
+    options: {
+      'title': 'Acessos nos últimos 7 dias'
     }
+  }
 }
 
 
 const trafficByPlatformLast7Days = {
-    query: {
-        'start-date': '7daysAgo',
-        'end-date': 'yesterday',
-        'metrics': 'ga:pageviews',
-        'dimensions': 'ga:pagePathLevel1',
-        'sort': '-ga:pageviews',
-        'filters': 'ga:pagePathLevel1!=/',
-        'max-results': 4 //number of platforms
-    },
-    chart: {
-        'type': 'PIE',
-        'options': {
-            'width': '100%',
-            'pieHole': 4 / 9,
-            'title': 'Tráfego por plataforma nos últimos 7 dias'
-        }
+  query: {
+    'start-date': '7daysAgo',
+    'end-date': 'yesterday',
+    'metrics': 'ga:pageviews',
+    'dimensions': 'ga:pagePathLevel1',
+    'sort': '-ga:pageviews',
+    'filters': 'ga:pagePathLevel1!=/',
+    'max-results': 4 //number of platforms
+  },
+  chart: {
+    'type': 'PIE',
+    'options': {
+      'width': '100%',
+      'pieHole': 4 / 9,
+      'title': 'Tráfego por plataforma nos últimos 7 dias'
     }
+  }
 }
 
 // analytics views ID
 const views = {
-    query: {
-        ids: "ga:125230257"
-    }
+  query: {
+    ids: "ga:125230257"
+  }
 }
 
 function getCurrentDate() {
-    let newDate = new Date()
-    let date = newDate.getDate();
-    let month = newDate.getMonth() + 1;
-    let year = newDate.getFullYear();
+  let newDate = new Date()
+  let date = newDate.getDate();
+  let month = newDate.getMonth() + 1;
+  let year = newDate.getFullYear();
 
-    return `${year}-${month < 10 ? `0${month}` : `${month}`}-${date}`
+  return `${year}-${month < 10 ? `0${month}` : `${month}`}-${date}`
 }
 
 export default class AnalyticsPage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            token: "",
-            startDate: '2016-01-01',
-            endDate: getCurrentDate(),
-            lineChartData: {
-                query: {
-                    'start-date': '2016-01-01',
-                    'end-date': 'yesterday',
-                    'metrics': 'ga:pageviews,ga:sessions',
-                    'dimensions': 'ga:year',
-                },
-                chart: {
-                    'type': 'LINE',
-                    'options': {
-                        'title': 'E-democracia - últimos anos'
-                    }
-                }
-            },
-            pieChartData: {
-                query: {
-                    'start-date': '2016-01-01',
-                    'end-date': 'yesterday',
-                    'metrics': 'ga:pageviews',
-                    'dimensions': 'ga:pagePathLevel1',
-                    'sort': '-ga:pageviews',
-                    'filters': 'ga:pagePathLevel1!=/',
-                    'max-results': 4 //number of platforms
-                },
-                chart: {
-                    'type': 'PIE',
-                    'options': {
-                        'width': '100%',
-                        'pieHole': 4 / 9,
-                        'title': 'Tráfego por plataforma  nos últimos anos'
-                    }
-                }
-            },
-        };
-        this.chartIDs = ['edemocracia-30days', 'edemocracia-7days', 'trafficByPlatformLast7Days']
-        this.singleImagePromiseById = this.singleImagePromiseById.bind(this)
-        this.mountsAllPromisesFromSingleImages = this.mountsAllPromisesFromSingleImages.bind(this)
-        this.appendsImagesToPDFFile = this.appendsImagesToPDFFile.bind(this)
-        this.generatesAllChartsPDF = this.generatesAllChartsPDF.bind(this)
-        this.exportFinalFile = this.exportFinalFile.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleStartDateChange = this.handleStartDateChange.bind(this);
-        this.handleEndDateChange = this.handleEndDateChange.bind(this);
-    }
-
-    componentDidMount = () => {
-        const url = new URL('http://localhost:5000')
-        fetch(url, {
-            method: 'GET',
-        })
-            .then(response => response.json())
-            .then((fetchToken) => {
-                this.setState({ token: fetchToken.token }); // TODO: handle errors
-            });
-    }
-
-    singleImagePromiseById(id) {
-        const input = document.getElementById(id)
-        return html2canvas(input).then((canvas) => {
-            return canvas.toDataURL('image.png')
-        })
-    }
-
-    mountsAllPromisesFromSingleImages() {
-        var promises = []
-        for (var i = 0; i < this.chartIDs.length; i++) {
-            var p = this.singleImagePromiseById(this.chartIDs[i])
-            promises.push(p)
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: "",
+      startDate: '2016-01-01',
+      endDate: getCurrentDate(),
+      lineChartData: {
+        query: {
+          'start-date': '2016-01-01',
+          'end-date': 'yesterday',
+          'metrics': 'ga:pageviews,ga:sessions',
+          'dimensions': 'ga:year',
+        },
+        chart: {
+          'type': 'LINE',
+          'options': {
+            'title': 'Acessos'
+          }
         }
-        return promises
-    }
+      },
+      pieChartData: {
+        query: {
+          'start-date': '2016-01-01',
+          'end-date': 'yesterday',
+          'metrics': 'ga:pageviews',
+          'dimensions': 'ga:pagePathLevel1',
+          'sort': '-ga:pageviews',
+          'filters': 'ga:pagePathLevel1!=/',
+          'max-results': 4 //number of platforms
+        },
+        chart: {
+          'type': 'PIE',
+          'options': {
+            'width': '100%',
+            'pieHole': 4 / 9,
+            'title': 'Tráfego por plataforma'
+          }
+        }
+      },
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleStartDateChange = this.handleStartDateChange.bind(this);
+    this.handleEndDateChange = this.handleEndDateChange.bind(this);
+  }
 
-    generatesAllChartsPDF() {
-        var promises = this.mountsAllPromisesFromSingleImages()
-        Promise.all(promises).then((images) => {
-            this.appendsImagesToPDFFile(images)
-        })
-    }
+  componentDidMount = () => {
+    const url = new URL('http://localhost:5000') // Alterar pela URL válida 
+    fetch(url, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then((fetchToken) => {
+        this.setState({ token: fetchToken.token }); // TODO: handle errors
+      });
+  }
 
-    appendsImagesToPDFFile(images, single_image_id) {
-        const pdf = new jsPDF('p', 'px', 'a4')
-        var width = pdf.internal.pageSize.getWidth()
-        console.log("images length: " + images.length)
-        images.forEach((image, index) => {
-            var imgProps = pdf.getImageProperties(image)
-            var height = (imgProps.height * width) / imgProps.width
-            pdf.addImage(image, 'JPEG', 0, 0, width, height)
-            if (index < images.length - 1) pdf.addPage()
-        })
-        single_image_id === null ? this.exportFinalFile({
-            'name': 'all_charts.pdf',
-            'pdf': pdf
-        }) : this.exportFinalFile({
-            'name': single_image_id,
-            'pdf': pdf
-        })
-    }
+  handleSubmit(event) {
+    this.setState(prevState => ({
+      lineChartData: {
+        ...prevState.lineChartData,
+        query: {
+          ...prevState.query,
+          'start-date': this.state.startDate,
+          'end-date': this.state.endDate,
+          'metrics': 'ga:pageviews,ga:sessions',
+          'dimensions': 'ga:yearMonth',
+        }
+      },
+      pieChartData: {
+        ...prevState.pieChartData,
+        query: {
+          ...prevState.query,
+          'start-date': this.state.startDate,
+          'end-date': this.state.endDate,
+          'metrics': 'ga:pageviews',
+          'dimensions': 'ga:pagePathLevel1',
+          'sort': '-ga:pageviews',
+          'filters': 'ga:pagePathLevel1!=/',
+          'max-results': 4
+        }
+      }
+    }))
+    event.preventDefault();
+  }
 
-    exportFinalFile(obj) {
-        obj.pdf.save(obj.name)
-    }
+  handleStartDateChange(date) {
+    this.setState({ startDate: date.toISOString().split('T')[0] })
+  }
 
-    printSingleChart(id) {
-        this.singleImagePromiseById(id)
-            .then((image) => {
-                this.appendsImagesToPDFFile([image], id)
-            })
-    }
+  handleEndDateChange(date) {
+    this.setState({ endDate: date.toISOString().split('T')[0] })
+  }
 
-    handleSubmit(event) {
-        this.setState(prevState => ({
-            lineChartData: {
-                ...prevState.lineChartData,
-                query: {
-                    ...prevState.query,
-                    'start-date': this.state.startDate,
-                    'end-date': this.state.endDate,
-                    'metrics': 'ga:pageviews,ga:sessions',
-                    'dimensions': 'ga:yearMonth',
-                }
-            },
-            pieChartData: {
-                ...prevState.pieChartData,
-                query: {
-                    ...prevState.query,
-                    'start-date': this.state.startDate,
-                    'end-date': this.state.endDate,
-                    'metrics': 'ga:pageviews',
-                    'dimensions': 'ga:pagePathLevel1',
-                    'sort': '-ga:pageviews',
-                    'filters': 'ga:pagePathLevel1!=/',
-                    'max-results': 4
-                }
-            }
-        }))
-        event.preventDefault();
-    }
-
-    handleStartDateChange(date) {
-        this.setState({ startDate: date.toISOString().split('T')[0] })
-    }
-
-    handleEndDateChange(date) {
-        this.setState({ endDate: date.toISOString().split('T')[0] })
-    }
-
-    render() {
-        return (
-            <ResponsiveDrawer title='Dashboard'>
-                <center>
-                    <GoogleProvider accessToken={this.state.token}>
-                        <h2> E-democracia - últimos 30 dias </h2>
-                        <div className="chart" id='edemocracia-30days'>
-                            <GoogleDataChart views={views} config={last30days} />
-                        </div>
-                        <button onClick={() => this.printSingleChart('edemocracia-30days')}> Download </button>
-                        <div className="chart" id='trafficByPlatformLast30Days'>
-                            <GoogleDataChart views={views} config={trafficByPlatformLast30Days} />
-                        </div>
-                        <br />
-                        <h2> E-democracia - últimos 7 dias </h2>
-                        <div className="chart" id='edemocracia-7days'>
-                            <GoogleDataChart views={views} config={last7days} />
-                        </div>
-                        <button onClick={() => this.printSingleChart('edemocracia-7days')}> Download </button>
-                        <div className="chart" id='trafficByPlatformLast7Days'>
-                            <GoogleDataChart views={views} config={trafficByPlatformLast7Days} />
-                        </div>
-                        <button onClick={() => this.printSingleChart('trafficByPlatformLast7Days')}> Download </button>
-                        <br />
-                        <h2> Selecione o período para consulta </h2>
-                        <form onSubmit={this.handleSubmit}>
-                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                <DatePicker
-                                    id="start-date"
-                                    name="startDate"
-                                    label="Start Date"
-                                    type="date"
-                                    format="yyyy-MM-dd"
-                                    value={this.state.startDate}
-                                    onChange={this.handleStartDateChange}
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                />
-                                <DatePicker
-                                    id="end-date"
-                                    name="endDate"
-                                    label="End Date"
-                                    type="date"
-                                    format="yyyy-MM-dd"
-                                    value={this.state.endDate}
-                                    onChange={this.handleEndDateChange}
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                />
-                            </MuiPickersUtilsProvider>
-                            <input type="submit" value="Enviar" />
-                        </form>
-                        <GoogleDataChart views={views} config={this.state.lineChartData} />
-                        <GoogleDataChart views={views} config={this.state.pieChartData} />
-                        <button onClick={this.generatesAllChartsPDF}> Download All </button>
-                    </GoogleProvider>
-                </center>
-            </ResponsiveDrawer >
-        )
-    }
+  render() {
+    return (
+      <ResponsiveDrawer title='Dashboard'>
+        <GoogleProvider accessToken={this.state.token}>
+          <h2> E-democracia - últimos 30 dias </h2>
+          <Paper>
+            <Box m={1}>
+              <Grid container>
+                <Grid item xs={6}>
+                  <GoogleDataChart views={views} config={last30days} />
+                </Grid>
+                <Grid item xs={6}>
+                  <GoogleDataChart views={views} config={trafficByPlatformLast30Days} />
+                </Grid>
+              </Grid>
+            </Box>
+          </Paper>
+          <h2> E-democracia - últimos 7 dias </h2>
+          <Paper>
+            <Box m={1}>
+              <Grid container>
+                <Grid item xs={6}>
+                  <GoogleDataChart views={views} config={last7days} />
+                </Grid>
+                <Grid item xs={6}>
+                  <GoogleDataChart views={views} config={trafficByPlatformLast7Days} />
+                </Grid>
+              </Grid>
+            </Box>
+          </Paper>
+          <h2> Selecione o período para consulta </h2>
+          <form onSubmit={this.handleSubmit}>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DatePicker
+                id="start-date"
+                name="startDate"
+                label="Start Date"
+                type="date"
+                format="yyyy-MM-dd"
+                value={this.state.startDate}
+                onChange={this.handleStartDateChange}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <DatePicker
+                id="end-date"
+                name="endDate"
+                label="End Date"
+                type="date"
+                format="yyyy-MM-dd"
+                value={this.state.endDate}
+                onChange={this.handleEndDateChange}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </MuiPickersUtilsProvider>
+            <input type="submit" value="Enviar" />
+          </form>
+          <Paper>
+            <Box m={1}>
+              <Grid container>
+                <Grid item xs={6}>
+                  <GoogleDataChart views={views} config={this.state.lineChartData} />
+                </Grid>
+                <Grid item xs={6}>
+                  <GoogleDataChart views={views} config={this.state.pieChartData} />
+                </Grid>
+              </Grid>
+            </Box>
+          </Paper>
+        </GoogleProvider>
+      </ResponsiveDrawer >
+    )
+  }
 }
