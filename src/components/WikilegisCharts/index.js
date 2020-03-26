962014550c4eab9cb5be254014fdd1af27b71b3f
Teogenes Moura <mourateogenes@gmail.com>
2020-03-26 17:31:03 -0300

    grafico wikilegis documentos por tema (#16)
    * grafico wikilegis documentos por tema
    
    * added a wikilegis chart for gender
    
    * gender wikilegis chart
    
    Co-authored-by: Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>

---
+++ src/components/WikilegisCharts/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { GoogleDataChart } from 'react-analytics-widget'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Title, Legend, BarSeries, PieSeries, Chart, ArgumentAxis, ValueAxis, LineSeries } from "@devexpress/dx-react-chart-material-ui";
+
+
+class WikilegisCharts extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ done: true });
+    }, 2500);
+  }
+
+  render() {
+    return (
+      <div>
+          <Chart
+            data={[
+              { tema: "Esportes", quant: 1},
+              { tema: "Comunicações", quant: 2},
+              { tema: "Meio Ambiente" , quant: 1},
+              { tema: "Cultura", quant: 1},
+              { tema: "Segurança", quant: 1}
+            ]}
+          > 
+            <ArgumentAxis />
+            <ValueAxis />
+            <BarSeries valueField="quant" argumentField="tema" />
+            <Title text="Documentos por tema no Wikilegis" />
+          </Chart>
+
+          <br />
+          <br />
+
+                    <Chart
+            data={[
+              { gender: "masc", quant: 1212},
+              { gender: "fem", quant: 3201},
+            ]}
+          > 
+            <ArgumentAxis />
+            <ValueAxis />
+            <PieSeries valueField="quant" argumentField="gender" />
+            <Title text="Usuários por gênero no Wikilegis" />
+          </Chart>
+    </div>
+    )
+  }
+}
+
+export default WikilegisCharts;
