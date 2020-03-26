cb3bd6e67f0281b9875a95b9967b280f40f07e94
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2020-03-26 13:51:25 -0300

    Adding new side menu layout (#13)

--- src/containers/MenuDrawer/listItems.js
+++ src/containers/MenuDrawer/listItems.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import PhoneIcon from '@material-ui/icons/Phone';
-import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
-import ListIcon from '@material-ui/icons/List';
-
 import { Link } from "react-router-dom";
 
 
 export const toolsListItens = (
-  <div>
-    <ListSubheader inset>Gráficos</ListSubheader>
-
+  <div> 
     <ListItem button component={Link}
       to={"/"}>
       <ListItemIcon>
@@ -22,27 +15,5 @@ export const toolsListItens = (
       </ListItemIcon>
       <ListItemText primary="Página Inicial" />
     </ListItem>
-
-    <ListItem button component={Link}>
-      <ListItemIcon>
-        <ListIcon />
-      </ListItemIcon>
-      <ListItemText primary="Enquetes" />
-    </ListItem>
-
-    <ListItem button component={Link}>
-      <ListItemIcon>
-        <PhoneIcon />
-      </ListItemIcon>
-      <ListItemText primary="0800" />
-    </ListItem>
-
-    <ListItem button>
-      <ListItemIcon>
-        <QuestionAnswerIcon />
-      </ListItemIcon>
-      <ListItemText primary="Plenarinho" />
-    </ListItem>
-
   </div>
 );
