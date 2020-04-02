ff58cf8e53cc6d51d942e82a63376a4f96f17298
Thiago Nogueira <thiagonf10@gmail.com>
2020-04-02 16:07:03 -0300

    Adding cards from numbers votes, suggestions and users from Enquetes

---
+++ src/components/EnquetesCardSuggestions/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import ForumIcon from "@material-ui/icons/Forum";
+import Typography from "@material-ui/core/Typography";
+
+export class EnquetesCardSuggestions extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Paper elevation={3} square={false} rounded={5}>
+          <Box marginX={2} marginY={2}>
+            <Grid container>
+              <Grid item xs={2}></Grid>
+              <Grid item xs={3}>
+                <ForumIcon style={{ fontSize: 70 }} />
+              </Grid>
+              <Grid item>
+                <Box marginTop={1}>
+                  <Typography component="p" variant="h4">
+                    {this.props.suggestions}
+                  </Typography>
+                  <Typography color="textSecondary">
+                    Total de sugestões
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+        </Paper>
+      </React.Fragment>
+    );
+  }
+}
+
+export default EnquetesCardSuggestions;
