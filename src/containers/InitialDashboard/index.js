7a679f0d0877a64dac423fe99a7ed91ff402d137
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2020-04-03 12:04:46 -0300

    added welcome message to the initial page of the platform

--- src/containers/InitialDashboard/index.js
+++ src/containers/InitialDashboard/index.js
@@ -87,9 +87,14 @@ class InitialDashboard extends Component {
         <ResponsiveDrawer title = 'Dashboard'>
           <Paper>
             <Box style={{padding: '1rem'}}>
-              <center><h1>Dados da participação pública da Câmara dos Deputados</h1></center>
-              <center><img src="/img/initial_dashboard.svg" height="600" width="600" /></center>
-              <p style={{fontSize: '1.2rem', lineHeight: '1.5', padding: '1rem', textAlign: 'justify'}}> Bem vindo a plataforma de participação pública da Câmara dos Deputados. </p>
+              <center><h1>Dados da participação pública da Câmara dos Deputados</h1>
+              <img src="/img/initial_dashboard.svg" height="500" width="500" />
+              <h2 style={{lineHeight: '1.5', padding: '1rem', textAlign: 'justify'}}><center> Bem vindo a plataforma de participação pública da Câmara dos Deputados! </center></h2>
+              <p style={{fontSize: '1.2rem', lineHeight: '2.5', padding: '0 5rem 0 5rem', textAlign: 'justify'}}> Neste portal, nós disponibilizamos os dados em forma gráfica e textual das plataformas de participação da Câmara dos Deputados.
+                Como ainda estamos em fase de protótipo, os dados mostrados neste momento <b> não são reais </b> mas representam dados que podem ser obtidos das plataformas numa fase futura da
+                plataforma. 
+                </p> <p style={{fontSize: '1.2rem', lineHeight: '1.5', padding: '1rem', textAlign: 'justify'}}><center> Agradecemos a sua colaboração!</center></p>
+              </center>
             </Box>
           </Paper>
         </ResponsiveDrawer>
